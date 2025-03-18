b9f25678ea7a8f77e7280b7deb34f7d6a82d7b6c
Hilda Aileen Raymundo Díaz <hraymundod@miumg.edu.gt>
2025-03-18 17:44:59 -0600

    Se cargan las interfaces principales

---
+++ SistemaDeTickets.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sistemadetickets;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hraym
+ */
+public class SistemaDeTickets extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage formulario) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Usuario.fxml"));
+        formulario.setTitle("Login");
+        formulario.setScene(new Scene(root));
+        formulario.show();
+    }
+        
+}
